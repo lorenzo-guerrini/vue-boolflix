5dc9288018d51b0695a37e12b3c8023aa4ebe4e2
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-08 00:07:53 +0100

    Installato fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 import Flag from "vue-flagpack";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as fasStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(farStar);
+library.add(fasStar);
+
+Vue.component("fas", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 Vue.use(Flag, {
   name: "Flag",
